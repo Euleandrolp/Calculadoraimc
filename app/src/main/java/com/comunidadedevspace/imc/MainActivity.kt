25e4ae9a4340c1e90437fc7160743cad245c786e
Leandro <nandolimapr925@gmail.com>
2024-08-28 17:03:01 +0100

    Mostrando Classificacao ao usuario

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -10,12 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Recuperar os componentes EditTest
-        // Criar uma variavel e associar (=) o componente de UI<EditTest>
-        // Recuperar botao da tela
-        // Colocar acao no botao
-        // Recuperar o texto digitado no peso
 
         val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
@@ -30,7 +25,7 @@ class MainActivity : AppCompatActivity() {
 
 
             if(pesoStr == "" || alturaStr == "" ) {
-            // Mostrar mensagem para o usuario!
+
                 Snackbar
                     .make(
                         edtPeso,
@@ -45,6 +40,15 @@ class MainActivity : AppCompatActivity() {
                val alturaQ2 = altura * altura
                val resultado = peso / alturaQ2
 
+                // Navegar para proxima tela
+                // Criar o layout da proxima tela
+                // Passar dados (resultado) para proxima tela
+
+                // Intent - Classe do proprio android
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
 
                println("Ação do botão "+ resultado)
 
